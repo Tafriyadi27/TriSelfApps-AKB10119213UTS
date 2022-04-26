86fbb5363f67a3032cd351528668fd0c3809ed52
Tafriyadi27 <87406088+Tafriyadi27@users.noreply.github.com>
2022-04-26 10:06:22 +0700

    adding navigation in bottom nav bar

--- app/src/main/java/com/skullbreker/triselfapps/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/skullbreker/triselfapps/ui/gallery/GalleryFragment.java
@@ -1,4 +1,4 @@
-package com.skullbreker.triselfapps.ui.notifications;
+package com.skullbreker.triselfapps.ui.gallery;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,21 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.skullbreker.triselfapps.databinding.FragmentNotificationsBinding;
+import com.skullbreker.triselfapps.databinding.FragmentGalleryBinding;
 
-public class NotificationsFragment extends Fragment {
+public class GalleryFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentGalleryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        GalleryViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
+        final TextView textView = binding.textGallery;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
--- app/src/main/java/com/skullbreker/triselfapps/ui/music/MusicViewModel.java
+++ app/src/main/java/com/skullbreker/triselfapps/ui/music/MusicViewModel.java
@@ -1,16 +1,16 @@
-package com.skullbreker.triselfapps.ui.dashboard;
+package com.skullbreker.triselfapps.ui.music;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class MusicViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public MusicViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is music fragment");
     }
 
     public LiveData<String> getText() {
