6de8361ba3baab4e14de241376446099f63933ca
Tafriyadi27 <87406088+Tafriyadi27@users.noreply.github.com>
2022-05-02 23:22:17 +0700

    adding content in contact_fragment(fragment profile)

--- app/src/main/java/com/skullbreker/triselfapps/ui/profile/ContactFragment.java
+++ app/src/main/java/com/skullbreker/triselfapps/ui/profile/ContactFragment.java
@@ -1,7 +1,11 @@
 package com.skullbreker.triselfapps.ui.profile;
 
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,11 +15,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.skullbreker.triselfapps.R;
+import com.skullbreker.triselfapps.databinding.ContactFragmentBinding;
+import com.skullbreker.triselfapps.databinding.FragmentMusicBinding;
+import com.skullbreker.triselfapps.databinding.FragmentProfileBinding;
 
 public class ContactFragment extends Fragment {
-
+    private ContactFragmentBinding binding;
     private ContactViewModel mViewModel;
 
     public static ContactFragment newInstance() {
@@ -25,14 +33,36 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.contact_fragment, container, false);
+        binding = ContactFragmentBinding .inflate(inflater, container, false);
+        TextView waText = binding.textwa;
+        waText.setOnClickListener(view -> {
+            String url = "https://api.whatsapp.com/send?phone=+6285156632447";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        });
+        TextView emailText = binding.textmail;
+        emailText.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto","skullbreaker@gmail.com", null));
+            startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+        });
+        TextView igText = binding.textig;
+        igText.setOnClickListener(view -> {
+            Uri uri = Uri.parse("https://instagram.com/_u/quicklybubble158");
+            Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+            likeIng.setPackage("com.instagram.android");
+
+            try {
+                startActivity(likeIng);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://instagram.com/quicklybubble158")));
+            }
+        });
+        return binding.getRoot();
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ContactViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
 }
